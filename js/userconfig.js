ad2148bc4f0b5cb05df3e8aac5c082fe98157de0
Mike Boos <mike.boos@gmail.com>
2017-12-14 15:52:25 -0500

    Customize site for Waterloo Region.

--- js/userconfig.js
+++ js/userconfig.js
@@ -4,16 +4,16 @@
 
 var config = {
 	//Set Map Bounds & point map is centered around
-	mapFocus : [42.381899, -71.122499],
-	south : 42.24 ,
-	west : -71.27,
-	north : 42.453,
-	east : -70.95,
+	mapFocus : [43.440036, -80.4659329],
+	south : 43.339662 ,
+	west : -80.610809,
+	north : 43.547055,
+	east : -80.242767,
 	//Mapbox access token & key for basemap
 	mapboxAccessToken :              
-            'pk.eyJ1IjoicmVtb3RlZ2VudHJpZnkiLCJhIjoiY2lnanJzMjJpMDA1dnYxbHo5MTZtdGZsYSJ9.gLE8d40zmDAtMSSZyd2h1Q',
+            'pk.eyJ1IjoibWJvb3MiLCJhIjoiY2piNnlsdGlrMHhoNTJxbXoxZDUwbGxmdyJ9.Lq62YCNmAVLru7tw2NssdQ',
     //Change to your username, insert function on cartodb, and cartodb tablename (see also /cartoDB_functions)
-	cartoDBusername : 'raphaeld',
+	cartoDBusername : 'mboos',
 	cartoDBinsertfunction : 'insert_bikeways_data',
 	walkthroughWelcome: "<p>This webmap allows you to view data collected by the <a href='http://bostoncyclistsunion.org/' target='_blank'>Boston Cyclists Union</a> on biking in Boston.</p><p>It also allows you to submit input on where biking infrastructure can be improved by drawing on the map! The map is restricted to the inner core of Metro Boston, where we concentrate our advocacy.</p>"
 };
