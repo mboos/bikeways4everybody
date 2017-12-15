ccc85a1b7b4960a6e5d5fb86f078f6c0221087c7
Mike Boos <mike.boos@gmail.com>
2017-12-14 21:43:10 -0500

    Update bounds and message

--- js/userconfig.js
+++ js/userconfig.js
@@ -5,15 +5,15 @@
 var config = {
 	//Set Map Bounds & point map is centered around
 	mapFocus : [43.440036, -80.4659329],
-	south : 43.339662 ,
-	west : -80.610809,
-	north : 43.547055,
-	east : -80.242767,
+	south : 43.33 ,
+	west : -80.62,
+	north : 43.55,
+	east : -80.24,
 	//Mapbox access token & key for basemap
 	mapboxAccessToken :              
             'pk.eyJ1IjoibWJvb3MiLCJhIjoiY2piNnlsdGlrMHhoNTJxbXoxZDUwbGxmdyJ9.Lq62YCNmAVLru7tw2NssdQ',
     //Change to your username, insert function on cartodb, and cartodb tablename (see also /cartoDB_functions)
 	cartoDBusername : 'mboos',
 	cartoDBinsertfunction : 'insert_bikeways_data',
-	walkthroughWelcome: "<p>This webmap allows you to view data collected by the <a href='http://bostoncyclistsunion.org/' target='_blank'>Boston Cyclists Union</a> on biking in Boston.</p><p>It also allows you to submit input on where biking infrastructure can be improved by drawing on the map! The map is restricted to the inner core of Metro Boston, where we concentrate our advocacy.</p>"
+	walkthroughWelcome: "<p>This webmap allows you to view data collected by the <a href='http://tritag.ca/' target='_blank'>TriTAG</a> on biking in Waterloo Region.</p><p>It also allows you to submit input on where you would like to see a minimum grid cycling network by drawing on the map! The map is restricted to the urban areas of the Region.</p>"
 };
